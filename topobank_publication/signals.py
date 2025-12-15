40caa86f99c364ea5766040cd4c13df605163931
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-12-15 18:32:09 +0100

    BUG: Fix reading offsets

--- topobank_publication/signals.py
+++ topobank_publication/signals.py
@@ -9,16 +9,17 @@ from .models import Publication, PublicationCollection
 
 _log = logging.getLogger(__name__)
 
-# Offset for short_url encoding to avoid conflicts with existing DOIs
-# Configure via settings.SHORT_URL_OFFSET (default: 0)
-SHORT_URL_OFFSET = getattr(settings, 'SHORT_URL_OFFSET', 0)
+
+def _get_short_url_offset():
+    """Get the short_url offset from settings (read dynamically for testing)."""
+    return getattr(settings, 'SHORT_URL_OFFSET', 0)
 
 
 @receiver(post_save, sender=Publication)
 def set_short_url(sender, instance, created, **kwargs):
     """Set short_url on newly created publications."""
     if created and instance.short_url is None:
-        instance.short_url = short_url.encode_url(instance.id + SHORT_URL_OFFSET)
+        instance.short_url = short_url.encode_url(instance.id + _get_short_url_offset())
         instance.save(update_fields=['short_url'])
 
 
@@ -26,5 +27,5 @@ def set_short_url(sender, instance, created, **kwargs):
 def set_short_url_publication_collection(sender, instance, created, **kwargs):
     """Set short_url on newly created publication collections."""
     if created and instance.short_url is None:
-        instance.short_url = short_url.encode_url(instance.id + SHORT_URL_OFFSET)
+        instance.short_url = short_url.encode_url(instance.id + _get_short_url_offset())
         instance.save(update_fields=['short_url'])
