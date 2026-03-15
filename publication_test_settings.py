bfb5f8cb911470b95d440250b2e46049f20a6758
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2026-03-15 11:21:03 +0100

    TST: Respect environment settings

--- publication_test_settings.py
+++ publication_test_settings.py
@@ -1,12 +1,16 @@
-from test_settings import *
+import environ
 
-INSTALLED_APPS = INSTALLED_APPS + [
+from test_settings import *  # noqa: F403
+
+env = environ.Env()
+
+INSTALLED_APPS = INSTALLED_APPS + [  # noqa: F405
     "topobank_publication.apps.PublicationAppConfig",
 ]
 
 ROOT_URLCONF = "publication_test_urls"
 
-DATACITE_USERNAME = "test"
-DATACITE_PASSWORD = "test"
-DATACITE_API_URL = "https://api.test.datacite.org"
-PUBLICATION_DOI_PREFIX = "10.12345"
+DATACITE_USERNAME = env("DATACITE_USERNAME", default="test")
+DATACITE_PASSWORD = env("DATACITE_PASSWORD", default="test")
+DATACITE_API_URL = env("DATACITE_API_URL", default="https://api.test.datacite.org")
+PUBLICATION_DOI_PREFIX = env("PUBLICATION_DOI_PREFIX", default="10.12345")
