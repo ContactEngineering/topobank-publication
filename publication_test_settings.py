39a6627339d5d1b77a7d3971b8186df51e1e8903
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2026-03-15 11:29:58 +0100

    TST: Added missing anonymous user middleware

--- publication_test_settings.py
+++ publication_test_settings.py
@@ -10,6 +10,8 @@ INSTALLED_APPS = INSTALLED_APPS + [  # noqa: F405
 
 ROOT_URLCONF = "publication_test_urls"
 
+MIDDLEWARE += ["topobank.middleware.anonymous_user_middleware"]  # noqa: F405
+
 DATACITE_USERNAME = env("DATACITE_USERNAME", default="test")
 DATACITE_PASSWORD = env("DATACITE_PASSWORD", default="test")
 DATACITE_API_URL = env("DATACITE_API_URL", default="https://api.test.datacite.org")
