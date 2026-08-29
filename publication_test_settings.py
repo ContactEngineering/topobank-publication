cec574da6e2acbc65f3665aa34c1135231dc067c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2026-08-29 14:43:44 +0200

    TST: Add noqa comment for flake8

--- publication_test_settings.py
+++ publication_test_settings.py
@@ -10,7 +10,7 @@ INSTALLED_APPS = INSTALLED_APPS + [  # noqa: F405
 
 ROOT_URLCONF = "publication_test_urls"
 
-MIDDLEWARE += ["topobank.testing.mock_auth.users.middleware.anonymous_user_middleware"]
+MIDDLEWARE += ["topobank.testing.mock_auth.users.middleware.anonymous_user_middleware"]  # noqa: F405
 
 
 DATACITE_USERNAME = env("DATACITE_USERNAME", default="test")
